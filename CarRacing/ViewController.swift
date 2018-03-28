de90807630eecbe2e4518b35e05a44cb0c3e2e51
Sumite Ramchande <admin@Sumites-MacBook-Pro.local>
2018-03-28 18:01:54 +0100

    Implemented background animation

--- CarRacing/ViewController.swift
+++ CarRacing/ViewController.swift
@@ -10,7 +10,43 @@ import UIKit
 class ViewController: UIViewController {
     
  
+    @IBOutlet weak var roadImage: UIImageView!
+    
+    var dynamicAnimator: UIDynamicAnimator!
+    var dynamicItemBehavior: UIDynamicItemBehavior!
+    
+    
+    @IBOutlet weak var car: UIImageView!
+    let cars = ["car1.png","car2.png", "car3.png"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
+        //Animation of walking
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!,
+                      UIImage(named: "road20.png")!]
+        
+        roadImage.image = UIImage.animatedImage(with: imageArray, duration: 1)
     }
     
     override func didReceiveMemoryWarning() {
@@ -18,7 +54,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func randomImgPicker() {
+        let randomNumber = Int(arc4random_uniform(UInt32(cars.count))) // generating random number
+        car.image = UIImage(named: cars[randomNumber])
+    }
     
 }
 
