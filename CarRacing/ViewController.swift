478ba416ef97e9f9af0c99d14b6a8515882c60a2
Sumite Ramchande <admin@Sumites-MacBook-Pro.local>
2018-03-28 17:54:32 +0100

    Attempt to make cars to fall

---
+++ CarRacing/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  CarRacing
+//
+//  Created by Sumite Ramchande on 21/03/2018.
+//  Copyright © 2018 Sumite Ramchande. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+ 
+    @IBOutlet weak var roadImage: UIImageView!
+    
+    var dynamicAnimator: UIDynamicAnimator!
+    var dynamicItemBehavior: UIDynamicItemBehavior!
+    
+    
+    @IBOutlet weak var car: UIImageView!
+    let cars = ["car1.png","car2.png", "car3.png"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Animation of walking
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!,
+                      UIImage(named: "road20.png")!]
+        
+        roadImage.image = UIImage.animatedImage(with: imageArray, duration: 1)
+       
+        
+        
+        randomImgPicker() // getting a random image
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+            
+        dynamicItemBehavior = UIDynamicItemBehavior(items: [car])
+        self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: 300), for: car)
+        dynamicAnimator.addBehavior(dynamicItemBehavior)
+        for index in 0...4{
+            
+            let delay = Double(self.carArray[index])
+            
+            let when = DispatchTime.now() + delay
+            
+            DispatchQueue.main.asyncAfter(deadline: when){
+                let carView = UIImageView(image:nil)
+                carView.image = self.carArray[index]
+                carView.frame = CGRect(x: 1, y: 2, width: 30 , height: 50)
+                self.view.addSubview(carView)
+                
+                dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+                
+                dynamicItemBehaviour = UIDynamicItemBehavior(items: [carView])
+                self.dynamicItemBehaviour(CGPoint(x: 0,y: 300), for:carView)
+                dynamicAnimator.addBehavior(dynamicItemBehaviour)
+            }
+        }
+    }
+    
+    
+
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func randomImgPicker() {
+        let randomNumber = Int(arc4random_uniform(UInt32(cars.count))) // generating random number
+        car.image = UIImage(named: cars[randomNumber])
+    }
+    
+}
+
