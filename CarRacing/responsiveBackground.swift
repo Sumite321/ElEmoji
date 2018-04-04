3c224de5f0114f0696c0e7a06dba9fb3e4a7f600
Sumite Ramchande <admin@Sumites-MacBook-Pro.local>
2018-04-04 15:49:36 +0100

    Implemented Game over screen and resposive background

---
+++ CarRacing/responsiveBackground.swift
@@ -0,0 +1,9 @@
+//
+//  responsiveBackground.swift
+//  CarRacing
+//
+//  Created by Sumite Ramchande on 04/04/2018.
+//  Copyright © 2018 Sumite Ramchande. All rights reserved.
+//
+
+import Foundation
