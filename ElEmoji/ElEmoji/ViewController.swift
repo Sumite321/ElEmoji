57239a85ad5da15fb904337559d4b9ae5accc035
Sumite Ramchande <admin@Sumites-MacBook-Pro.local>
2018-04-19 21:21:30 +0100

    Project rename to ElEmoji

---
+++ ElEmoji/ElEmoji/ViewController.swift
@@ -0,0 +1,559 @@
+//
+//  ViewController.swift
+//  CarRacing
+//
+//  Created by Sumite Ramchande on 21/03/2018.
+//  Copyright © 2018 Sumite Ramchande. All rights reserved.
+
+import UIKit
+import Foundation
+import AVFoundation
+
+//This protocol explains what actions this delegate will involve.
+protocol helperDelegate {
+    func setUpCollisionBoundary()
+}
+
+
+class ViewController: UIViewController, helperDelegate{
+    
+    let beepSoundURL =  Bundle.main.url(forResource: "gameplay", withExtension: "wav")!
+    let hitBy =  Bundle.main.url(forResource: "lost", withExtension: "wav")!
+    let gameOverUrl =  Bundle.main.url(forResource: "gameover", withExtension: "wav")!
+    let secondLeftUrl =  Bundle.main.url(forResource: "sec", withExtension: "wav")!
+    
+    var beepPlayer = AVAudioPlayer()
+    var lostPlayer = AVAudioPlayer()
+    var gameOver = AVAudioPlayer()
+    var secondLeft = AVAudioPlayer()
+    var sv: UIView!
+    
+    @IBOutlet weak var player: DraggedImageView!
+    
+    //@IBOutlet weak var timerText: UITextField!
+    var score = 0
+    
+    //obstacle cars variables
+    var obstacleCars: [UIImage]!
+    
+    // timer, game loop
+    var timer: Timer!
+    var timer1: Timer!
+    var timer3: Timer!
+    var gameOverImage: UIImageView!
+    var timerColl: Timer!
+    var playAgainButton:UIButton!
+    var playButton:UIButton!
+    let homeImageTop = UIImageView(frame: CGRect(x:30, y:40, width:30, height:40))
+    let soundIcon = UIImageView(frame: CGRect(x:70, y:45, width:30, height:30))
+
+    var count = 20
+    var i = 0
+    var popup:UILabel!
+    var showScore:UILabel!
+    var showBonus:UILabel!
+    var showTotal:UILabel!
+
+    var showTimer:UILabel!
+    var scoreText:UILabel!
+    var bonus:UILabel!
+    var showAppTitle:UILabel!
+    //Behaviour variables
+    var dynamicAnimator: UIDynamicAnimator!
+    var collisionBehavior: UICollisionBehavior!
+    var dynamicItemBehavior: UIDynamicItemBehavior!
+    var ended = false
+    var mute = false
+    
+    func setUpCollisionBoundary() {
+        // clear out all the boundaries from the behaviour
+        collisionBehavior.removeAllBoundaries()
+        // create a boundary around the player frame
+        collisionBehavior.addBoundary(withIdentifier: "boundary" as
+            NSCopying, for: UIBezierPath(rect: player.frame))
+    }
+    
+    
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addHomeBackground()
+        drawPlayButton()
+        drawHomeImage()
+        drawSoundIcon()
+        self.view.addLogoImage()
+        
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @objc func startObstacleAnimation() -> Void {
+        
+        
+        let tillThree = Int(arc4random_uniform(3))
+        //Randomely select how many cars will appear at the same time out of three
+        for _ in 0...tillThree {
+            
+            //add all the obstacles cars to the display
+            let obstacle = UIImageView(image: nil)
+            
+            score += 5
+            
+            let random = Int(arc4random_uniform(UInt32(243))) + 53
+            let randomWidth = Int(arc4random_uniform(UInt32(15))) + 30
+            let c = Int(arc4random_uniform(5))
+            
+            if(c==0){obstacle.tag = 80}else{obstacle.tag = 50}
+            
+            obstacle.image = obstacleCars[c]
+            obstacle.frame = CGRect(x: random, y: 0, width: randomWidth, height: 60)
+            self.view.addSubview(obstacle)
+        
+            dynamicItemBehavior.addItem(obstacle)
+            
+            //Make the obstacle come down
+            // random speeds, some faster some slower
+            let speed = Int(arc4random_uniform(240)) + 120
+            dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: speed), for: obstacle)
+            dynamicAnimator.addBehavior(dynamicItemBehavior)
+            
+            collisionBehavior.addItem(obstacle)
+            
+            //add the behaviour to the dynamic animator
+            dynamicAnimator.addBehavior(collisionBehavior)
+           
+            
+            
+        }
+        
+    }
+    
+    func gameOver(timer: Timer) -> Void {
+        
+        if(!ended){
+        timer.invalidate() //Stop the loop that calls the startObstacleAnimation()
+        timer1.invalidate()
+        beepPlayer.stop()
+        gameOverSound()
+        gameOverImage = UIImageView(image: nil)
+        gameOverImage.image = UIImage(named: "gameO.jpeg")
+        gameOverImage.frame = UIScreen.main.bounds
+        gameOverImage.addBlurEffect()
+        self.view.addSubview(gameOverImage)
+        
+        self.view.bringSubview(toFront: gameOverImage)
+        
+        playAgainButton = UIButton(frame: CGRect(x: UIScreen.main.bounds.size.width - 130, y: UIScreen.main.bounds.size.height - 90, width: 100, height: 50))
+        playAgainButton.backgroundColor = .orange
+        playAgainButton.layer.cornerRadius = 0.2 * playAgainButton.bounds.size.width
+        playAgainButton.clipsToBounds = true
+        playAgainButton.setTitle("Play Again", for: .normal)
+        playAgainButton.backgroundColor = .clear
+            
+            playAgainButton.layer.borderWidth = 1
+            playAgainButton.layer.borderColor = UIColor.black.cgColor
+        playAgainButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        //playAgainButton.center = self.view.center
+//        // customise the view
+        showScore = UILabel(frame: CGRect(x:width/6, y:height/2, width:250, height:51))
+        showBonus = UILabel(frame: CGRect(x:width/6, y:height/2 + 20, width:250, height:51))
+        showTotal = UILabel(frame: CGRect(x:width/6, y:height/2 + 60, width:250, height:51))
+        
+            showScore.text = " Your  score: " + scoreText.text!
+            showBonus.text = " Your  bonus: " +  bonus.text!
+            //var total = Int(scoreText.text!) + Int(bonus.text!)
+            
+            let a:Int? = Int(scoreText.text!)// firstText is UITextField
+            let b:Int? = Int(bonus.text!)// secondText is UITextField
+            let total = a! + b!
+            showTotal.text = " Total score:  " + String(total)
+            //showScore.font = showScore.font.withSize(40)
+            //showScore.backgroundColor = .orange
+
+        showScore.font = UIFont(name: "Verdana-Bold", size: 12.0)
+        showScore.textColor = UIColor.white
+        showScore.font = showScore.font.withSize(20)
+        
+        showBonus.font = UIFont(name: "Verdana-Bold", size: 12.0)
+        showBonus.textColor = UIColor.white
+        showBonus.font = showScore.font.withSize(20)
+
+        showTotal.textColor = UIColor.white
+        showTotal.font = showScore.font.withSize(23)
+        
+        // show on screen
+        self.view.addSubview(showScore)
+        self.view.addSubview(showBonus)
+        self.view.addSubview(showTotal)
+        self.view.addSubview(playAgainButton)
+        }
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        for view in self.view.subviews{
+            print(view.tag)
+            if(view.tag <= 100){
+            view.removeFromSuperview()
+            }
+        }
+        //self.view.removeFromSuperview();
+        playAgainButton.removeFromSuperview();
+        gameOverImage.removeFromSuperview();
+        showScore.removeFromSuperview()
+        showBonus.removeFromSuperview()
+        showTotal.removeFromSuperview()
+        scoreText.removeFromSuperview()
+        bonus.removeFromSuperview()
+        startAgain()
+    }
+    
+    @objc func buttonPlay(sender: UIButton!){
+        //self.view.removeLogo()
+        //let sv = UIViewController.displaySpinner(onView: self.view)
+        
+        runGameLoop()
+        
+    }
+    
+    func startAgain(){
+        
+        score = 0
+        i = 0
+        count = 20
+        timer3.invalidate()
+        playMySound()
+        runGameLoop()
+    }
+    
+    func runGameLoop(){
+        
+        
+        self.view.removeHomeImage()
+        playButton.removeFromSuperview()
+        self.view.removeLogo()
+        
+        self.view.addBackground()
+        //UIViewController.removeSpinner(spinner: sv)
+        // score at the bottom
+        scoreText = UILabel(frame: CGRect(x:UIScreen.main.bounds.size.width-90, y:UIScreen.main.bounds.size.height - 80, width:180, height:31))
+        scoreText.font = scoreText.font.withSize(40)
+        
+        self.view.addSubview(scoreText)
+        
+        scoreText.text = "0"
+        scoreText.tag = 300
+        //let sv = UIViewController.displaySpinner(onView: self.view)
+        // bonus score at the bottom
+        bonus = UILabel(frame: CGRect(x:90, y:UIScreen.main.bounds.size.height - 80, width:180, height:31))
+        bonus.font = scoreText.font.withSize(40)
+        bonus.textColor = .orange
+        
+        self.view.addSubview(bonus)
+        
+        bonus.text = "0"
+        bonus.tag = 350
+        
+        //Assign viewController.swift as the delegate for the car image view
+        changePlayerImage()
+        
+        // **********
+        player.myDelegate = self
+        player.tag = 200
+        //player.center = self.view.c
+        
+        
+        obstacleCars = [UIImage(named: "star.png")!,
+                        UIImage(named: "dynamite-hi.png")!,
+                        UIImage(named: "bomb17.png")!,
+                        UIImage(named: "dynamite-hi.png")!,
+                        UIImage(named: "bomb17.png")!
+        ]
+        
+
+        playMySound()
+        playLostSound()
+        // timer for obstacles to fall
+        let date = Date().addingTimeInterval(3.5)
+        timer = Timer(fireAt: date, interval: 1.1, target: self, selector: #selector(startObstacleAnimation), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer, forMode: RunLoopMode.commonModes)
+        
+        
+        // timer at the top
+        showTimer = UILabel(frame: CGRect(x:UIScreen.main.bounds.size.width/2-40, y:40, width:280, height:31))
+        showTimer.font = showTimer.font.withSize(40)
+        
+        self.view.addSubview(showTimer)
+        
+        
+        
+        // timer for continuos score update
+        let date1 = Date().addingTimeInterval(0.5)
+        timer1 = Timer(fireAt: date1, interval: 0, target: self, selector: #selector(calculateScore), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer1, forMode: RunLoopMode.commonModes)
+        
+        // game over screen
+        let when = DispatchTime.now() + 22
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            self.gameOver(timer: self.timer)
+        }
+        
+        // behaviours
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        dynamicItemBehavior = UIDynamicItemBehavior()
+        collisionBehavior = UICollisionBehavior()
+        
+        
+        
+        
+        // customise the view
+        popup = UILabel(frame: CGRect(x:0, y:0, width:280, height:31))
+        popup.center = self.view.center
+        popup.text = "You have 20 seconds"
+        popup.textColor = UIColor.white
+        popup.font = UIFont(name: "Verdana-Bold", size: 23.0)
+        popup.alpha = 0
+        
+        popup.fadeIn(completion: {
+            (finished: Bool) -> Void in
+            self.popup.fadeOut()
+        })
+        
+        
+        
+        // show on screen
+        self.view.addSubview(popup)
+        
+        // timer for alert
+        Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(self.dismissAlert), userInfo: nil, repeats: false)
+    }
+    
+    @objc func calculateScore(){
+  
+        for view in self.view.subviews{
+            if((view.tag == 50 || view.tag == 80) && player.frame.intersects(view.frame)){
+                if(view.tag == 80){view.removeFromSuperview()
+                    //score = score + 15
+                    var a:Int? = Int(bonus.text!)
+                    a = a! + 15
+                    bonus.text =  String(a!)
+                    player.image = UIImage(named:"staremoji.png")
+                }else{
+                //score = score - (i/1000)
+                i = i+1
+                //print(score)
+                //print("i is " ,i/500)
+                //print(score - (i/300))
+                player.image = UIImage(named:"redface.png")
+                scoreText.text = String(score - (i/40))
+                }
+                lostPlayer.play()
+                //playLostSound()
+            }
+        
+            
+                //view.removeFromSuperview()}
+            if(view.tag == 50 && view.frame.origin.y > UIScreen.main.bounds.size.height-5){
+                changePlayerImage()
+                view.removeFromSuperview()
+            }
+            scoreText.text = String(score - (i/40))
+            
+        }
+        
+    }
+    
+    @objc func update() {
+        if(count >= 0){
+            let minutes = String(count / 60)
+            let seconds = String((count % 60))
+            if(count < 5){
+                showTimer.textColor = UIColor.red
+                count -= 1
+                showTimer.text = minutes + ":" + seconds
+                secondLeftSound()
+            }else{
+            showTimer.text = minutes + ":" + seconds
+            count -= 1
+                showTimer.textColor = UIColor.white}
+        }
+}
+
+    
+    @objc func dismissAlert(){
+        if popup != nil { // Dismiss the view from here
+            popup.removeFromSuperview()
+            timer3 = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func changePlayerImage(){
+ 
+        var imageArray: [UIImage]!
+        imageArray = [UIImage(named: "frame1.gif")!,
+                      UIImage(named: "frame2.gif")!,
+                      UIImage(named: "frame3.gif")!,
+                      UIImage(named: "frame4.gif")!,
+                      UIImage(named: "frame5.gif")!,
+                      UIImage(named: "frame6.gif")!,
+                      UIImage(named: "frame7.gif")!,
+                      UIImage(named: "frame8.gif")!,
+                      UIImage(named: "frame9.gif")!,
+                      UIImage(named: "frame10.gif")!]
+        
+        
+        
+        //animate the road images
+        player.image = UIImage.animatedImage(with: imageArray, duration: 1.4)
+        
+        
+        
+        
+        // you can change the content mode:
+        player.contentMode = UIViewContentMode.scaleAspectFill
+        
+    }
+    
+    func playMySound(){
+        do {
+            beepPlayer = try! AVAudioPlayer(contentsOf: beepSoundURL)
+        } catch {
+            print(error)
+        }
+        beepPlayer.prepareToPlay()
+        beepPlayer.play()
+        beepPlayer.numberOfLoops = -1
+    }
+    
+    func playLostSound(){
+        do {
+            lostPlayer = try! AVAudioPlayer(contentsOf: hitBy)
+        } catch {
+            print(error)
+        }
+        lostPlayer.prepareToPlay()
+        lostPlayer.play()
+    }
+    
+    func gameOverSound(){
+        do {
+            gameOver = try! AVAudioPlayer(contentsOf: gameOverUrl)
+        } catch {
+            print(error)
+        }
+        gameOver.prepareToPlay()
+        gameOver.play()
+    }
+    
+    func secondLeftSound(){
+        do {
+            secondLeft = try! AVAudioPlayer(contentsOf: secondLeftUrl)
+        } catch {
+            print(error)
+        }
+        secondLeft.prepareToPlay()
+        secondLeft.play()
+    }
+    
+    func drawPlayButton(){
+
+        
+        playButton = UIButton(frame: CGRect(x: UIScreen.main.bounds.size.width - 130, y: UIScreen.main.bounds.size.height - 90, width: UIScreen.main.bounds.size.width/1.7, height: 50))
+        playButton.backgroundColor = .orange
+        playButton.layer.cornerRadius = 0.1 * playButton.bounds.size.width
+        playButton.clipsToBounds = true
+        playButton.setTitle("Play", for: .normal)
+        playButton.center = self.view.center
+        playButton.addTarget(self, action: #selector(buttonPlay), for: .touchUpInside)
+        playButton.alpha = 0.9
+        playButton.layer.borderWidth = 2
+        playButton.layer.borderColor = UIColor.black.cgColor
+        
+        self.view.addSubview(playButton)
+    }
+    
+//    @objc func tappedMe()
+//    {
+//        // remove all the objects
+//        for view in self.view.subviews{
+//            print(view.tag)
+//            if(view.tag <= 100){
+//                view.removeFromSuperview()
+//            }
+//        }
+//        // remove the animated image
+//        self.view.removeAnimatedImage()
+//        self.view.addHomeBackground()
+//
+//        showTimer.removeFromSuperview()
+//        showScore.removeFromSuperview()
+//
+//
+//        ended = true
+//
+//        // draw the play button
+//        drawPlayButton()
+//
+//
+//
+//
+//    }
+    
+    func drawHomeImage(){
+        
+        homeImageTop.image = UIImage(named:"home.png" )
+        homeImageTop.tag = 700
+        //let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tappedMe))
+        
+        //homeImageTop.addGestureRecognizer(tap)
+        homeImageTop.isUserInteractionEnabled = true
+        
+        // you can change the content mode:
+        homeImageTop.contentMode = UIViewContentMode.scaleAspectFill
+        
+        
+        self.view.addSubview(homeImageTop)
+        self.view.sendSubview(toBack: homeImageTop)
+    }
+    
+    func drawSoundIcon(){
+        
+        soundIcon.image = UIImage(named:"soundOn.png" )
+        soundIcon.tag = 800
+        let tap = UITapGestureRecognizer(target: self, action: #selector(muteSound))
+        
+        soundIcon.addGestureRecognizer(tap)
+        soundIcon.isUserInteractionEnabled = true
+        
+        // you can change the content mode:
+        soundIcon.contentMode = UIViewContentMode.scaleAspectFill
+        
+        
+        self.view.addSubview(soundIcon)
+        self.view.sendSubview(toBack: soundIcon)
+    }
+    
+    @objc func muteSound(){
+    
+        
+        if(mute){
+            soundIcon.image = UIImage(named :"soundOn.png")
+            beepPlayer.play()
+            mute = false
+        }else{
+        soundIcon.image = UIImage(named :"soundOff.png")
+        beepPlayer.stop()
+        mute = true
+        }
+        
+    }
+    
+}
---
+++ ElEmoji/ElEmoji/responsiveBackground.swift
@@ -0,0 +1,176 @@
+//
+//  responsiveBackground.swift
+//  CarRacing
+//
+//  Created by Sumite Ramchande on 04/04/2018.
+//  Copyright © 2018 Sumite Ramchande. All rights reserved.
+//
+
+import UIKit
+
+// screen width and height:
+let width = UIScreen.main.bounds.size.width
+let height = UIScreen.main.bounds.size.height
+let imageViewBackground = UIImageView(frame: CGRect(x:0, y:0, width:width, height:height))
+let homeImage = UIImageView(frame: CGRect(x:0, y:0, width:width, height:height))
+let logoImage = UIImageView(frame: CGRect(x:50, y:70, width:width-100, height:height/5))
+
+
+
+extension UIView {
+    
+   
+    
+    func removeHomeImage(){
+        
+            homeImage.removeFromSuperview()
+        
+    }
+    
+    func addHomeBackground(){
+        homeImage.image = UIImage(named:"emoji.jpg")
+        homeImage.tag = 600
+        
+        
+        
+        // you can change the content mode:
+        homeImage.contentMode = UIViewContentMode.scaleAspectFill
+        self.addSubview(homeImage)
+        self.sendSubview(toBack: homeImage)
+    }
+    
+    func addBackground() {
+      
+        
+        //let imageViewBackground = UIImageView(frame: CGRect(x:0, y:0, width:width, height:height))
+        //imageViewBackground.image = UIImage(named: "YOUR IMAGE NAME GOES HERE")
+        
+        //Create an array
+        //and add all the road images to it
+        var imageArray: [UIImage]!
+        imageArray = [
+            UIImage(named: "c80.gif")!,
+            UIImage(named: "c79.gif")!,
+            UIImage(named: "c78.gif")!,
+            UIImage(named: "c77.gif")!,
+            UIImage(named: "c76.gif")!,
+            UIImage(named: "c75.gif")!,
+            UIImage(named: "c74.gif")!,
+            UIImage(named: "c73.gif")!,
+            UIImage(named: "c72.gif")!,
+            UIImage(named: "c71.gif")!,
+            UIImage(named: "c70.gif")!,
+            UIImage(named: "c69.gif")!,
+            UIImage(named: "c68.gif")!,
+            UIImage(named: "c67.gif")!,
+            UIImage(named: "c66.gif")!,
+            UIImage(named: "c65.gif")!,
+            UIImage(named: "c64.gif")!,
+            UIImage(named: "c63.gif")!,
+            UIImage(named: "c62.gif")!,
+            UIImage(named: "c61.gif")!,
+            UIImage(named: "c60.gif")!,
+            UIImage(named: "c59.gif")!,
+            UIImage(named: "c58.gif")!,
+            UIImage(named: "c57.gif")!,
+            UIImage(named: "c56.gif")!,
+            UIImage(named: "c55.gif")!,
+            UIImage(named: "c54.gif")!,
+            UIImage(named: "c53.gif")!,
+            UIImage(named: "c52.gif")!,
+            UIImage(named: "c51.gif")!,
+            UIImage(named: "c50.gif")!,
+            UIImage(named: "c49.gif")!,
+            UIImage(named: "c48.gif")!,
+            UIImage(named: "c47.gif")!,
+            UIImage(named: "c46.gif")!,
+                      UIImage(named: "c45.gif")!,
+                      UIImage(named: "c44.gif")!,
+                      UIImage(named: "c43.gif")!,
+                      UIImage(named: "c42.gif")!,
+                      UIImage(named: "c41.gif")!,
+                      UIImage(named: "c40.gif")!,
+                      UIImage(named: "c39.gif")!,
+                      UIImage(named: "c38.gif")!,
+                      UIImage(named: "c37.gif")!,
+                      UIImage(named: "c36.gif")!,
+                      UIImage(named: "c35.gif")!,
+                      UIImage(named: "c34.gif")!,
+                      UIImage(named: "c33.gif")!,
+                      UIImage(named: "c32.gif")!,
+                      UIImage(named: "c31.gif")!,
+                      UIImage(named: "c30.gif")!,
+                      UIImage(named: "c29.gif")!,
+                      UIImage(named: "c28.gif")!,
+                      UIImage(named: "c27.gif")!,
+                      UIImage(named: "c26.gif")!,
+                      UIImage(named: "c25.gif")!,
+                      UIImage(named: "c24.gif")!,
+                        UIImage(named: "c23.gif")!,
+                      UIImage(named: "c22.gif")!,
+                      UIImage(named: "c21.gif")!,
+                      UIImage(named: "c20.gif")!,
+                      UIImage(named: "c19.gif")!,
+                      UIImage(named: "c18.gif")!,
+                      UIImage(named: "c17.gif")!,
+                      UIImage(named: "c16.gif")!,
+                      UIImage(named: "c15.gif")!,
+                    UIImage(named: "c14.gif")!,
+                    UIImage(named: "c13.gif")!,
+                    UIImage(named: "c12.gif")!,
+                    UIImage(named: "c11.gif")!,
+                    UIImage(named: "c10.gif")!,
+                    UIImage(named: "c9.gif")!,
+                    UIImage(named: "c8.gif")!,
+                    UIImage(named: "c7.gif")!,
+                    UIImage(named: "c6.gif")!,
+                    UIImage(named: "c5.gif")!,
+                    UIImage(named: "c4.gif")!,
+                    UIImage(named: "c3.gif")!,
+                    UIImage(named: "c2.gif")!,
+                    UIImage(named: "c1.gif")!
+        ]
+        
+        
+        
+        //animate the road images
+        imageViewBackground.image = UIImage.animatedImage(with: imageArray, duration: 3.4)
+        imageViewBackground.tag = 300
+        
+        
+        
+        // you can change the content mode:
+        imageViewBackground.contentMode = UIViewContentMode.scaleAspectFill
+        
+        self.addSubview(imageViewBackground)
+        self.sendSubview(toBack: imageViewBackground)
+    }
+    
+    func removeAnimatedImage(){
+        
+        imageViewBackground.removeFromSuperview()
+    }
+
+    func addLogoImage(){
+        
+        logoImage.image = UIImage(named:"logo1.png" )
+        logoImage.tag = 650
+        
+        
+        
+        // you can change the content mode:
+        //logoImage.contentMode = UIViewContentMode.scaleAspectFill
+        self.addSubview(logoImage)
+        self.sendSubview(toBack: logoImage)
+        self.bringSubview(toFront: logoImage)
+    }
+    
+    func removeLogo(){
+        
+        logoImage.removeFromSuperview()
+        
+    }
+    
+}
+
+
